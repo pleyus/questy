d8d5fa57c6ce32e9d46776d3e96a9c6e2baaab69
pleyus <pleyus@gmail.com>
2018-09-30 20:48:52 -0500

    Se maquetó el encabezado de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { PanelComponent } from './panel/panel.component';
+import { HeaderComponent } from './panel/header/header.component';
 
+const appRoutes: Routes = [
+  { path: 'view', component: ViewerComponent },
+  { path: 'panel', component: PanelComponent },
+  { path: '', redirectTo: '/panel', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewerComponent,
+    PanelComponent,
+    HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot( appRoutes ),
     BrowserModule
   ],
   providers: [],
